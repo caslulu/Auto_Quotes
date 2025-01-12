67ba4798754e056e2bf942ca071d3f49ae9a5d4c
Lucas Duarte <lucasbatistasuarte@gmail.com>
2025-01-11 20:10:47 -0800

    Banner Completo

---
+++ Canva_Banner/preco_automatico.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+SIZE_PRECO = 55
+SIZE_SEGURADORA = 40
+SIZE_ASSOCIADO = 45
+
+
+
+class PrecoAutomatico:
+    def __init__(self):
+        self.basico = Image.open("Imagens/basico.png")
+        self.completo = Image.open("Imagens/full.png")
+        self.fonte_preco = ImageFont.truetype("Fonte/fonte.otf", size=SIZE_PRECO)
+        self.fonte_seguradora = ImageFont.truetype("Fonte/fonte.otf", size=SIZE_SEGURADORA)
+        self.fonte_associado = ImageFont.truetype("Fonte/fonte.otf", size=SIZE_ASSOCIADO)
+
+    def escrever_preco_basico(self, entrada, mensalidade, a_vista, associado, seguradora):
+        
+        draw = ImageDraw.Draw(self.completo)
+        seguradora = draw.text((800, 562), seguradora, font=self.fonte_seguradora, fill="white")
+        entrada = draw.text((870, 1400), entrada, font=self.fonte_preco, fill="black")
+        mensalidade = draw.text((885, 1545), mensalidade, font=self.fonte_associado, fill="black")
+        a_vista = draw.text((850, 1695), a_vista, font=self.fonte_preco, fill="black")
+        associado = draw.text((490, 1908), associado, font=self.fonte_associado, fill="white")
+        self.completo.save("Imagens/completo_feito.png")
+
+
+
+
+preco = PrecoAutomatico()
+preco.escrever_preco_basico(seguradora="Geico", entrada="398.38", associado="Lucas Batista - 2 Veiculos", mensalidade="5x398.38", a_vista="3398.38")
